f3a397b17bb28b5b134cb8c0feebda22072e3e4b
Liat Keshet <liat-ke@ynet.co.il>
2022-04-13 15:21:19 +0300

    Add project files.

---
+++ UserTestApi/Controllers/UserController.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UserTestApi.Models;
+
+namespace UserTestApi.Controllers
+{
+    [EnableCors()]
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {
+        private static List<User> UsersList = new List<User>()
+        {
+            new User
+            {
+                ID = 1,
+                UserName = "LiatKe",
+                FirstName = "Liat",
+                LastName = "Danos",
+                BirthDate = new DateTime(1998,04,30)
+            },
+               new User
+            {
+                ID = 2,
+                UserName = "AdiKe",
+                FirstName = "Alk",
+                LastName = "Danos",
+                BirthDate = new DateTime(1981,02,01)
+            },
+                  new User
+            {
+                ID = 3,
+                UserName = "RotemKeshet",
+                FirstName = "rk",
+                LastName = "Keshet",
+                BirthDate = new DateTime(1975,01,02)
+            }
+        };
+
+        [Route("{id}")]
+        [HttpGet]
+        public IActionResult Get(int id)
+        {
+            User user = UsersList.Where(x => x.ID == id).FirstOrDefault();
+            return Ok(user);
+        }
+
+        [HttpGet]
+        public List<User> Get()
+        {
+            return UsersList;
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] User User)
+        {
+            if (id != User.ID)
+                return BadRequest(new { message = "ID Does Not Exists" });
+
+            User currentUser = UsersList.Where(x => x.ID == id).FirstOrDefault();
+            if (!string.IsNullOrEmpty(User.FirstName) || !string.IsNullOrEmpty(User.LastName))
+            {
+                if (User.UserName.ToLower().Contains(User.FirstName.ToLower()) || User.UserName.ToLower().Contains(User.LastName.ToLower()))
+                    return BadRequest("User Name Can Not Includes Your First Name Or Last Name");
+            }
+            currentUser.UserName = User.UserName;
+            currentUser.LastName = User.LastName;
+            currentUser.FirstName = User.FirstName;
+            currentUser.BirthDate = User.BirthDate;
+            return Ok(currentUser);
+        }
+    }
+}
---
+++ UserTestApi/Startup.cs
@@ -0,0 +1,88 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UserTestApi
+{
+    public class Startup
+    {
+        readonly string MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddControllers();
+            string[] allowedOrigins = { "https://localhost:3000" };
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy",
+                    builder => builder.AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader());
+            });
+            //services.AddCors(options =>
+            //{
+            //    options.AddPolicy(MyAllowSpecificOrigins, builder =>
+            //    builder
+            //    .SetIsOriginAllowedToAllowWildcardSubdomains()
+            //    .WithOrigins(allowedOrigins)
+            //    .AllowAnyMethod()
+            //    .AllowAnyHeader()
+            //    .AllowCredentials().Build());
+            //});
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+               
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseCors(MyAllowSpecificOrigins);
+
+            //app.UseCors(builder =>
+            //{
+            //    builder
+            //    .AllowAnyOrigin()
+            //    .AllowAnyMethod()
+            //    .SetIsOriginAllowed(origin => true) // allow any origin
+            //    .AllowCredentials()
+            //    .AllowAnyHeader();
+            //});
+
+            app.UseCors("CorsPolicy");
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
